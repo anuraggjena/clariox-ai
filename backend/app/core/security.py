eb3aa1c6ed4394a74acf1640b688e958cfa8e120
Anurag Jena <anuragjena14@gmail.com>
2026-02-16 22:12:19 +0530

    Fix bcrypt production issue

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -6,11 +6,14 @@ from .config import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
-def hash_password(password: str):
+def hash_password(password: str) -> str:
+    # bcrypt only supports 72 bytes
+    password = password[:72]
     return pwd_context.hash(password)
 
 
-def verify_password(plain_password, hashed_password):
+def verify_password(plain_password: str, hashed_password: str) -> bool:
+    plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
 
 
